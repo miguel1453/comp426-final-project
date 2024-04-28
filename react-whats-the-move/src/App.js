d928a2f809112981cefcc82a175b1a3b61a8c1f5
Miguel Villasenor <97707721+miguel1453@users.noreply.github.com>
2024-04-28 14:58:25 -0400

    Login and Signup components

--- react-whats-the-move/src/App.js
+++ react-whats-the-move/src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import EventList from './components/EventsList';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './components/Signup.js'; // Adjust the path based on your project structure
+import EventList from './components/EventsList.js';
+import Login from './components/Login.js';
+
 
 function App() {
-  return (
-    <div className="App">
-      <EventList />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<EventList />} /> 
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ react-whats-the-move/src/components/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Cookie from 'js-cookie';
+import '../App.css';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:3001/login', {
+                username: username,
+                password: password
+            });
+            console.log('Login successful:', response.data);
+            Cookie.set('userId', response.data.id, { expires: 7 });
+        } catch (err) {
+            console.error('Login failed:', err.response ? err.response.data : 'No response');
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleLogin} className='signup-form'>
+                <h2>Login</h2>
+                <label>Username:</label>
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+                <label>Password:</label>
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
