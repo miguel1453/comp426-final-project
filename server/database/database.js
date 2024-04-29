d61638300297137cffbfc3a7a09d4dad791c5b68
Miguel Villasenor <97707721+miguel1453@users.noreply.github.com>
2024-04-29 16:46:30 -0400

    Implements friend endpoints and db logic

--- server/database/database.js
+++ server/database/database.js
@@ -15,11 +15,18 @@ let db = new sqlite3.Database('./mydatabase.db', sqlite3.OPEN_READWRITE | sqlite
 
 // Function to initialize the database tables if they don't exist
 function initializeDb() {
+    db.run('PRAGMA foreign_keys = ON;', (err) => {
+        if (err) {
+            console.error('Error enabling foreign key constraint', err.message);
+        } else {
+            console.log('Foreign key constraint enabled');
+        }
+    });
     const query = `
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL UNIQUE,
-            password TEXT NOT NULL
+            username VARCHAR(200) NOT NULL UNIQUE,
+            password VARCHAR(200) NOT NULL
         );
     `;
 
@@ -30,6 +37,23 @@ function initializeDb() {
             console.log('Users table created or already exists');
         }
     });
+
+    const query2 = `
+        CREATE TABLE IF NOT EXISTS friends (
+            username VARCHAR(255) NOT NULL,
+            friend_username VARCHAR(255) NOT NULL,
+            PRIMARY KEY (username, friend_username),
+            Foreign Key (username) references users(username),
+            Foreign Key (friend_username) references users(username)
+        );`;
+    db.run(query2, (err) => {
+        if (err) {
+            console.error('Error creating friends table', err.message);
+        } else {
+            console.log('Friends table created or already exists');
+        }
+    });
+
 }
 
 // Add your API endpoints here
