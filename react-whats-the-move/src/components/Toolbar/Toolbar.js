eb536374a2cafdba438c15fd9231d509a5a52885
Miguel Villasenor <97707721+miguel1453@users.noreply.github.com>
2024-04-29 23:21:08 -0400

    Added ternary for profile / login

--- react-whats-the-move/src/components/Toolbar/Toolbar.js
+++ react-whats-the-move/src/components/Toolbar/Toolbar.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Box, IconButton } from '@mui/material';
-import SearchBar from '../SearchBar/SearchBar';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import LocalActivity from '@mui/icons-material/LocalActivity';
-import Feed from '../Feed/Feed';
 import { Link } from 'react-router-dom';
-import Profile from '../Profile/Profile';
 import styles from './Toolbar.module.css';
+import Cookies from 'js-cookie';
 
 function TopToolbar({ onFeedClick, onHomeClick }) {
+    const userId = Cookies.get('userId');
     return (
         <AppBar position="static" sx={{ backgroundColor: '#483C32' }}>
             <Toolbar>
@@ -17,9 +16,14 @@ function TopToolbar({ onFeedClick, onHomeClick }) {
                 <Typography variant="h5" component="div" sx={{ flexGrow: 1, fontFamily: 'Limelight, sans-serif', marginLeft: 0}}>
                     what's the move
                 </Typography>
-                {/* <SearchBar /> */}
-                <Link className={styles.toolbarTink} to="/profile">Your Feed</Link>
-                <Link className={styles.toolbarTink} to='/login'>Sign In/Register</Link>
+                { 
+                userId ? 
+                <Link className={styles.toolbarTink} to="/profile">Your Feed</Link> 
+                : 
+                <Link className={styles.toolbarTink} to='/login'>Sign In/Register</Link> 
+                }
+               
+                
             </Toolbar>
         </AppBar>
     );
